f3e6d4918383fa1ddb20d314975685b4feb55fa6
abhiagni11 <abhiagni11@users.noreply.github.com>
2018-06-02 21:54:38 -0700

    Update touch.ino

--- sketchbook/touch/touch.ino
+++ sketchbook/touch/touch.ino
@@ -1,20 +1,3 @@
-/*********************************************************
-This is a library for the MPR121 12-channel Capacitive touch sensor
-
-Designed specifically to work with the MPR121 Breakout in the Adafruit shop 
-  ----> https://www.adafruit.com/products/
-
-These sensors use I2C communicate, at least 2 pins are required 
-to interface
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada for Adafruit Industries.  
-BSD license, all text above must be included in any redistribution
-**********************************************************/
-
 #include <Wire.h>
 #include "Adafruit_MPR121.h"
 
